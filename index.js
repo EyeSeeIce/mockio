4856c6bbbf8538c499e01e224b67913a4e8185e9
Vladislav <vs@in4comgroup.com>
2022-02-25 17:42:02 +0300

    api keys

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(cookieParser('secret_key', {
   maxAge: 3600 * 24
 }))
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://mock-dev.ru/'],
+  origin: '*',
   credentials:true,
   withCredentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
