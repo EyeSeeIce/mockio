95a3bdb0db76d98c2c931fa4acd2482e01188118
Vladislav <vs@in4comgroup.com>
2022-02-26 13:34:36 +0300

    no cookies

--- index.js
+++ index.js
@@ -15,9 +15,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-app.use(cors({
-  origin: '*',
-}))
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 
 
 app.use('/api/', getMock)
