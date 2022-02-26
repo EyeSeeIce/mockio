2f9970b3d2e3e0c2d25c8cda3829664001fc4609
Vladislav <vs@in4comgroup.com>
2022-02-26 13:38:19 +0300

    no cookies

--- index.js
+++ index.js
@@ -9,19 +9,26 @@ const cookieParser = require("cookie-parser");
 const auth_middleware = require('./middlewares/auth_middleware')
 const app = express()
 
+const corsOpts = {
+  origin: '*',
+
+  methods: [
+    'GET',
+    'POST',
+  ],
+
+  allowedHeaders: [
+    'Content-Type',
+  ],
+};
+app.use(cors(corsOpts))
+
 require('dotenv').config({ path: `prod.env` })
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-app.use(function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
-});
 
 
 app.use('/api/', getMock)
