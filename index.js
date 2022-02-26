bd75e11ce8e9d507ee1274c3ffe00ca80d52be44
Vladislav <vs@in4comgroup.com>
2022-02-26 13:47:56 +0300

    no cookies

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const auth_middleware = require('./middlewares/auth_middleware')
 const app = express()
 
 const corsOpts = {
-  origin: '*',
+  origin: ['http://127.0.0.1', 'http://127.0.0.1:3000'],
 
   methods: [
     'GET',
