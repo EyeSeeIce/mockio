e3a9b072ee646c9d01968f78932226255fa5e97d
Vladislav <vs@in4comgroup.com>
2022-02-27 18:58:55 +0300

    auth_middleware

--- routes/api_key.js
+++ routes/api_key.js
@@ -5,20 +5,11 @@ const router = express.Router()
 const uuid = require('uuid')
 const auth_middleware = require("../middlewares/auth_middleware");
 
-router.get('/key/',auth_middleware, async (req, res) => {
-  try{
-    const {id} = req.user_data
-    const key = await client.q(`select api_key from mock_api_keys where user_id='${id}'`)
-    res.send(key)
-  }catch (e){
-    res.status(400).send({message: e.message})
-  }
-})
 
-router.post('/key/', async (req, res) => {
+
+router.get('/key/',auth_middleware, async (req, res) => {
   try {
     const key = uuid.v4()
-    console.log(req.user_data);
     const {id} = req.user_data
     await client.q(`insert into mock_api_keys (user_id, api_key) values ('${id}', '${key}')`)
     res.send(key)
